ae677e4268090b638b71ca6981141af7a2d434c4
mgv99 <gomezmarcos99@gmail.com>
2022-02-24 16:09:59 +0100

    add StructuredQuery.java

---
+++ src/main/java/com/xatkit/bot/structuredQuery/StructuredQuery.java
@@ -0,0 +1,65 @@
+package com.xatkit.bot.structuredQuery;
+
+import com.xatkit.bot.getResult.GetResult;
+import com.xatkit.bot.library.Intents;
+import com.xatkit.bot.library.Utils;
+import com.xatkit.execution.State;
+import com.xatkit.plugins.react.platform.ReactPlatform;
+import lombok.Getter;
+import lombok.val;
+
+import static com.xatkit.bot.Bot.coreLibraryI18n;
+import static com.xatkit.bot.Bot.getResult;
+import static com.xatkit.bot.Bot.messages;
+import static com.xatkit.bot.Bot.selectViewField;
+import static com.xatkit.bot.Bot.structuredFilter;
+import static com.xatkit.dsl.DSL.intentIs;
+import static com.xatkit.dsl.DSL.state;
+
+/**
+ * The Structured Query workflow of a chatbot.
+ * <p>
+ * It is the entry point or lobby of a set of workflows considered as structured. The workflows available from here are:
+ * <ul>
+ *     <li>{@link StructuredFilter}</li>
+ *     <li>{@link SelectViewField}</li>
+ *     <li>{@link GetResult}</li>
+ * </ul>
+ */
+public class StructuredQuery {
+
+    /**
+     * The entry point for the Structured Query workflow.
+     */
+    @Getter
+    private final State awaitingStructuredQueryState;
+
+    /**
+     * Instantiates a new Structured Query workflow.
+     *
+     * @param reactPlatform the react platform of a chatbot
+     * @param returnState   the state where the chatbot ends up arriving once the workflow is finished
+     */
+    public StructuredQuery(ReactPlatform reactPlatform, State returnState) {
+        val awaitingStructuredQueryState = state("AwaitingStructuredQuery");
+
+        awaitingStructuredQueryState
+                .body(context -> {
+                    reactPlatform.reply(context, messages.getString("SelectAction"),
+                            Utils.getFirstTrainingSentences(
+                                    Intents.addFilterIntent,
+                                    Intents.addFieldToViewIntent,
+                                    Intents.showDataIntent,
+                                    coreLibraryI18n.Quit));
+                })
+                .next()
+                .when(intentIs(Intents.addFilterIntent)).moveTo(structuredFilter.getSelectFieldState())
+                .when(intentIs(Intents.addFieldToViewIntent)).moveTo(selectViewField.getSelectViewFieldState())
+                .when(intentIs(Intents.showDataIntent)).moveTo(getResult.getGenerateResultSet())
+                .when(intentIs(coreLibraryI18n.Quit)).moveTo(returnState);
+
+
+        this.awaitingStructuredQueryState = awaitingStructuredQueryState.getState();
+    }
+
+}
