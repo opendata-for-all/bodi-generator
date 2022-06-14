b1590e4ff6558206fcf2c42b71985815886594f4
mgv99 <gomezmarcos99@gmail.com>
2022-06-14 12:23:45 +0200

    add DeployBotController.java

---
+++ src/main/java/bodi/generator/ui/controller/user/DeployBotController.java
@@ -0,0 +1,78 @@
+package bodi.generator.ui.controller.user;
+
+import bodi.generator.dataSchema.DataSchema;
+import bodi.generator.dataSchema.SchemaField;
+import bodi.generator.dataSchema.SchemaType;
+import bodi.generator.dataSource.TabularDataSource;
+import bodi.generator.ui.model.BodiGeneratorObjects;
+import bodi.generator.ui.model.Properties;
+import bodi.generator.ui.service.DashboardService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.ByteArrayInputStream;
+
+import static bodi.generator.BodiGenerator.createBot;
+
+/**
+ * The controller for the {@code deploy_bot} functionalities of the bodi-generator UI.
+ */
+@Controller
+@RequestMapping("/bodi-generator/deploy_bot")
+public class DeployBotController {
+
+    /**
+     * The shared objects between the controllers.
+     */
+    private final BodiGeneratorObjects objects;
+
+    /**
+     * The dashboard service of the controller.
+     */
+    private final DashboardService dashboard = new DashboardService();
+
+    /**
+     * Creates a new {@link DeployBotController}.
+     *
+     * @param objects the objects
+     */
+    public DeployBotController(BodiGeneratorObjects objects) {
+        this.objects = objects;
+    }
+
+    /**
+     * Show the {@code import_data} page.
+     *
+     * @param model the model
+     * @return the name of the page
+     */
+    @GetMapping("")
+    public String showDeployBot(Model model) {
+        model.addAttribute("dataImported", objects.isDataImported());
+        return dashboard.view(DashboardView.DEPLOY_BOT, model);
+    }
+
+    /**
+     * Download the bot as a zip file.
+     *
+     * @param response the response that will contain the zip file
+     */
+    @PostMapping("/download_bot_zip")
+    public void downloadBotZipEndpoint(HttpServletResponse response) {
+        if (objects.isDataImported()) {
+            TabularDataSource tds = objects.getTds();
+            DataSchema ds = objects.getDs();
+            byte[] csv = objects.getCsv();
+            Properties properties = objects.getProperties();
+            SchemaType schemaType = objects.getSchemaType();
+            for (SchemaField deletedSchemaField : schemaType.getDeletedSchemaFields()) {
+                tds.removeColumn(deletedSchemaField.getOriginalName());
+            }
+            createBot(properties, ds, new ByteArrayInputStream(csv), response);
+        }
+    }
+}
