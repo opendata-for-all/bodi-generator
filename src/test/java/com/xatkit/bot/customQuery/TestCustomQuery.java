4b341789f67ef24e0122822a043b994a8d5f420c
mgv99 <gomezmarcos99@gmail.com>
2023-01-05 21:10:32 +0100

    update TestCustomQuery.java
    modified to allow multiple bot testing simultaneously (for each bot initialized in the App)

--- src/test/java/com/xatkit/bot/customQuery/TestCustomQuery.java
+++ src/test/java/com/xatkit/bot/customQuery/TestCustomQuery.java
@@ -8,13 +8,13 @@ import com.opencsv.exceptions.CsvException;
 import com.xatkit.bot.App;
 import com.xatkit.bot.Bot;
 import com.xatkit.bot.nlp.NLPServerClient;
-import org.apache.commons.configuration2.BaseConfiguration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.text.MessageFormat;
 import java.util.List;
 import java.util.Map;
 
@@ -27,11 +27,6 @@ import static com.xatkit.testing.IntentMatchingTesting.testIntentMatching;
 public class TestCustomQuery {
 
     /**
-     * The chatbot used for testing.
-     */
-        private static Bot bot;
-
-    /**
      * The name of the file containing the utterances to test in Custom Query.
      * <p>
      * The structure of the csv file must be the following (i.e. the following must be the header of the csv file):
@@ -40,12 +35,11 @@ public class TestCustomQuery {
      * utterance_in_sql}, where the "detected" columns, "utterance_in_english" and "utterance_in_sql" are empty since
      * they will be filled through this process.
      */
-    private static String fileName;
+    private static final String fileName = "customQueryUtterances_{0}.csv";
 
     @BeforeAll
     static void setUpBeforeAll() {
-        bot = new Bot(new BaseConfiguration());
-        fileName = "customQueryUtterances_" + bot.language + ".csv";
+        App.initialize();
     }
 
     /**
@@ -55,21 +49,25 @@ public class TestCustomQuery {
      */
     @Test
     void testCustomQuery()  {
-        String outputFilePath = testIntentMatching(
-                bot.xatkitBot,
-                fileName,
-                bot.customQuery.getAwaitingCustomQueryState());
-        generateTranslations(App.nlpServerClient, outputFilePath);
+        for (Bot bot : App.bots) {
+            String botFilename = MessageFormat.format(fileName, bot.language);
+            String outputFilePath = testIntentMatching(
+                    bot.xatkitBot,
+                    botFilename,
+                    bot.customQuery.getAwaitingCustomQueryState());
+            generateTranslations(bot.language, App.nlpServerClient, outputFilePath);
+        }
     }
 
     /**
      * Creates a new file that contains the SQL translations for the utterances that were matched with the intent
      * "AnyValue", and the English translations for the utterances that, besides that, are not in English.
      *
+     * @param language        the language
      * @param nlpServerClient the client of the server that returns the translations
      * @param filePath        path of the file containing the utterances and detected intents
      */
-    private void generateTranslations(NLPServerClient nlpServerClient, String filePath) {
+    private void generateTranslations(String language, NLPServerClient nlpServerClient, String filePath) {
         // Rename "path/to/file/my_file.csv" to "path/to/file/my_fileSQL.csv"
         String outputFilePath = filePath.split("\\.")[0] + "SQL." + filePath.split("\\.")[1];
         try {
@@ -85,11 +83,11 @@ public class TestCustomQuery {
                 String detectedIntent = row[2];
                 if (detectedIntent.equals("AnyValue")) {
                     String utterance = row[0];
-                    Map<String, String> translations = nlpServerClient.getTranslations(utterance, bot.language);
+                    Map<String, String> translations = nlpServerClient.getTranslations(utterance, language);
                     String englishTranslation = translations.get("english");
                     String sqlTranslation = translations.get("sql");
 
-                    if (!bot.language.equals("en")) {
+                    if (!language.equals("en")) {
                         row[5] = englishTranslation;
                     }
                     row[6] = sqlTranslation;
